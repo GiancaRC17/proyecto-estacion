d8ac461d9c64dc4cf2230451f930ba24a9a41766
Giancarlo Yeshua <giancarlorodriguezcaldas1@gmail.com>
2026-08-30 17:17:59 -0500

    Initialize Spring Boot application structure

---
+++ src/main/java/com/estacion/ProyectoEstacionApplication.java
@@ -0,0 +1,12 @@
+package com.estacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEstacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoEstacionApplication.class, args);
+    }
+}
